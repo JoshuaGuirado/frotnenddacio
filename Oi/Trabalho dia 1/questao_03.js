c20eef19dcbdf9b0e2d4378f44d1943fffbafd0f
Joshua Guirado <joshuafguirado@gmail.com>
2026-05-04 21:57:03 -0300

    Delete Oi directory

--- Oi/Trabalho dia 1/questao_03.js
+++
@@ -1,7 +0,0 @@
-function mostrarTabela() {
-    var container = document.getElementById("tabela-container");
-    var img = document.createElement("img");
-    img.src = "img/Tabela_Jogos.png";
-    img.alt = "Tabela de Jogos da Copa";
-    container.appendChild(img);
-}
